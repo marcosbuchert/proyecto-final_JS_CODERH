524666dc5e8eaca7430bebbb63a9e85a24f33c3c
Marcos A. Büchert <49037687+marcosbuchert@users.noreply.github.com>
2022-02-21 12:43:45 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// La idea es que sea el comienzo de un Campus Virtual, donde los profesores puedan cargar notas y los alumnos puedan consultarlas
+
+var notas = []
+function cargar_notas(){
+    nombre = document.getElementById("nombre").value
+    apellido = document.getElementById("apellido").value
+    materia = document.getElementById("materia").value
+    nota = document.getElementById("nota").value
+
+    notas.push(new alumno(nombre, apellido, materia, nota))
+
+    alert("Se ha subido correctamente la nota a " + nombre + " " + apellido)
+}
+
+function alumno(nombre, apellido, materia, nota){
+    this.nombre = document.getElementById("nombre").value
+    this.apellido = document.getElementById("apellido").value
+    this.materia = document.getElementById("materia").value
+    this.nota = document.getElementById("nota").value
+}
+
+function nota_mayor(){
+   let max = 0
+
+    for ( let nota of notas ) {
+        if (max < nota.nota)
+            max = nota.nota;
+    }
+    alert("La nota mas alta es un " + max)
+}
+
+function calc_prom_grl(){
+    let prom = 0
+    let cont = 0
+    notas.forEach(element => {prom  = parseInt(prom) + parseInt(element.nota); cont++})
+    prom = prom/cont
+
+    if (prom != 0){
+        alert("El promedio general de las notas de los alumnos es: " + prom)
+    } else{
+        alert("No hay notas cargadas")
+    }
+}
