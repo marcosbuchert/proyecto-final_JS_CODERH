966d29fd83b506cac14277b3a904a8836f3cc2f0
Marcos A. Büchert <49037687+marcosbuchert@users.noreply.github.com>
2022-04-05 23:50:04 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,60 @@
-// La idea es que sea el comienzo de un Campus Virtual, donde los profesores puedan cargar notas y los alumnos puedan consultarlas
-
-var notas = []
-function cargar_notas(){
-    nombre = document.getElementById("nombre").value
-    apellido = document.getElementById("apellido").value
-    materia = document.getElementById("materia").value
-    nota = document.getElementById("nota").value
-
-    notas.push(new alumno(nombre, apellido, materia, nota))
-    swal({
-        title: "Se ha subido correctamente la nota a " + nombre + " " + apellido
-    })
-}
-
-function alumno(nombre, apellido, materia, nota){
+function Alumno(nombre, apellido, materia, nota){
     this.nombre = document.getElementById("nombre").value
     this.apellido = document.getElementById("apellido").value
     this.materia = document.getElementById("materia").value
     this.nota = document.getElementById("nota").value
 }
 
+function cargar_notas(){
+    nombre = document.getElementById("nombre").value
+    apellido = document.getElementById("apellido").value
+    materia = document.getElementById("materia").value
+    nota = document.getElementById("nota").value
+
+    let alumno = new Alumno(nombre, apellido, materia, nota)
+    let alumnos = JSON.parse(localStorage.getItem('Alumnos'))
+
+    if (alumnos == null){
+        localStorage.setItem('Alumnos', JSON.stringify(alumno))
+    } else {
+        alumnos.push(alumno)
+        localStorage.setItem('Alumnos', JSON.stringify(alumnos))
+    }
+    swal({ title: "Se ha subido correctamente la nota a " + nombre + " " + apellido})
+
+    mejor_promedio()
+}
+
+function mejor_promedio(){
+    let alumnos = JSON.parse(localStorage.getItem('Alumnos'))
+    let promedio = 0
+    alumnos.forEach(alumno => {
+        let persona = alumno.nombre + alumno.apellido
+        let prom = 0
+        let cont = 0
+        let mejor = ""
+        alumnos.forEach(_alumno => {
+            if (_alumno.nombre + _alumno.apellido == persona){
+                prom += parseInt(_alumno.nota)
+                cont++
+                mejor = _alumno.nombre + " " + _alumno.apellido
+            }
+        })
+        if (promedio < prom/cont){
+            mejor_alumno = mejor
+            promedio = prom/cont
+        }
+    })
+    document.getElementById("mejorPromedio").innerHTML = mejor_alumno + " es el/la mejor promedio, con una nota de " + promedio;
+}
+
 function nota_mayor(){
    let max = 0
+   let alumnos = JSON.parse(localStorage.getItem('Alumnos'))
 
-    for ( let nota of notas ) {
-        if (max < nota.nota)
-            max = nota.nota;
+    for (let alumno of alumnos) {
+        if (max < alumno.nota)
+            max = alumno.nota;
     }
     swal({
         title: "La nota mas alta es un " + max
@@ -35,7 +64,8 @@ function nota_mayor(){
 function calc_prom_grl(){
     let prom = 0
     let cont = 0
-    notas.forEach(element => {prom  = parseInt(prom) + parseInt(element.nota); cont++})
+    let alumnos = JSON.parse(localStorage.getItem('Alumnos'))
+    alumnos.forEach(alumno => {prom  = parseInt(prom) + parseInt(alumno.nota); cont++})
     prom = prom/cont
 
     if (prom != 0){
